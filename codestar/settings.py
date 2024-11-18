20514a66e3b84001f7cc500d330be069e5ae8d8e
Kate Eaves <3br90lcz@students.codeinstitute.net>
2024-11-18 13:29:30 +0000

    Add: About app

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-katepz-djangoblogci-sunx4d5nrpw.ws.codeinstitute-ide.net', '.herokuapp.com']
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_summernote',
     'blog',
+    'about',
 ]
 
 MIDDLEWARE = [
