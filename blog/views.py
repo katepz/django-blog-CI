47a4cf58240f6c895895e3ad724b222003d02cfc
Kate Eaves <3br90lcz@students.codeinstitute.net>
2024-11-20 10:14:00 +0000

    undo: error in views.py and Debug to false for deployment

--- blog/views.py
+++ blog/views.py
@@ -94,14 +94,3 @@ def comment_delete(request, slug, comment_id):
 
     return HttpResponseRedirect(reverse('post_detail', args=[slug]))    
 
-def test_successful_collaboration_request_submission(self):
-    """Test for a user requesting a collaboration"""
-    post_data = {
-        'name': 'test name',
-        'email': 'test@email.com',
-        'message': 'test message'
-    }
-    response = self.client.post(reverse('about'), post_data)
-    self.assertEqual(response.status_code, 200)
-    self.assertIn(
-        b'Collaboration request received! I endeavour to respond within 2 working days.', response.content)
